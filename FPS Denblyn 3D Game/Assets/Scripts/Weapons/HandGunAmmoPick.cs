0dfa2a35cfd2c440cdb0031b34565b9ac33dc3f8
hoanprd <99530243+hoanprd@users.noreply.github.com>
2023-11-11 23:09:26 +0700

    a

---
+++ FPS Denblyn 3D Game/Assets/Scripts/Weapons/HandGunAmmoPick.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandGunAmmoPick : MonoBehaviour
+{
+    public GameObject fakeAmmoClip;
+    public GameObject Message;
+    public AudioSource ammoPickUpSound;
+    public bool IsPickUp = false;
+
+    void Update()
+    {
+        /*if (IsPickUp == true && Input.GetKeyDown(KeyCode.F))
+        {
+            fakeAmmoClip.SetActive(false);
+            ammoPickUpSound.Play();
+            GetComponent<BoxCollider>().enabled = false;
+            GlobalAmmo.handgunAmmo += 7;
+            Message.SetActive(false);
+            IsPickUp = false;
+        }*/
+
+        if (IsPickUp == true && GlobalWeapons.PickUpItem == true)
+        {
+            GlobalWeapons.PickUpItem = false;
+            fakeAmmoClip.SetActive(false);
+            ammoPickUpSound.Play();
+            GetComponent<BoxCollider>().enabled = false;
+            GlobalAmmo.handgunAmmo += 7;
+            Message.SetActive(false);
+            IsPickUp = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GlobalWeapons.PickZone = true;
+        IsPickUp = true;
+        Message.SetActive(true);
+        Message.GetComponent<Text>().text = "Press 'F' to pick up";
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        GlobalWeapons.PickZone = false;
+        IsPickUp = false;
+        Message.SetActive(false);
+    }
+}
---
+++ FPS Denblyn 3D Game/Assets/Scripts/Weapons/HandGunPickUp.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandGunPickUp : MonoBehaviour
+{
+    public GameObject fakeHandGun;
+    public GameObject realHandGun;
+    public GameObject pistolImage;
+    public GameObject Message;
+    public AudioSource handGunPickUpSound;
+    public bool IsPickUp = false;
+
+    void Update()
+    {
+        /*if(IsPickUp == true && Input.GetKeyDown(KeyCode.F))
+        {
+            fakeHandGun.SetActive(false);
+            realHandGun.SetActive(true);
+            pistolImage.SetActive(true);
+            handGunPickUpSound.Play();
+            GetComponent<BoxCollider>().enabled = false;
+            Message.SetActive(false);
+            IsPickUp = false;
+            GlobalWeapons.HandGun = 1;
+        }*/
+
+        if (IsPickUp == true && GlobalWeapons.PickUpItem == true)
+        {
+            GlobalWeapons.PickUpItem = false;
+            fakeHandGun.SetActive(false);
+            realHandGun.SetActive(true);
+            pistolImage.SetActive(true);
+            handGunPickUpSound.Play();
+            GetComponent<BoxCollider>().enabled = false;
+            Message.SetActive(false);
+            IsPickUp = false;
+            GlobalWeapons.HandGun = 1;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GlobalWeapons.PickZone = true;
+        IsPickUp = true;
+        Message.SetActive(true);
+        Message.GetComponent<Text>().text = "Press 'F' to pick up";
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        GlobalWeapons.PickZone = false;
+        IsPickUp = false;
+        Message.SetActive(false);
+    }
+}
